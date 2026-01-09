d254b458482e3d54c07497772f7ff41f1f7dcda8
NITHIN KUMAR <131851611+nithinkumar2004@users.noreply.github.com>
2026-01-09 09:34:07 +0530

    Add toggleDetails function to show/hide project details

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleDetails(project) {
+    const details = project.querySelector(".details");
+    details.style.display =
+        details.style.display === "block" ? "none" : "block";
+}
